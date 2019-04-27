17fd547775e50c11b10afcdf96d93743fe7faa3c
rainflowe <187353113@qq.com>
2019-04-27 16:57:12 +0800

    Update band_demo_strategy.py

--- wingchun/strategy/py_demo/band_demo_strategy.py
+++ wingchun/strategy/py_demo/band_demo_strategy.py
@@ -44,32 +44,24 @@ class signal():
 
 	
 def run_test():
-    conn = pymysql.connect(host='cd-cdb-na7tl8h1.sql.tencentcdb.com', port=63889, user='root', passwd='5!Pd!Lgy',
-                           db='stock_data', charset='utf8')
-    #cursor
-    cursor = conn.cursor()
-    cursor.execute("select date,close,low from exc_hist_data limit 0,10000")
-    data = dp(cursor)
-    columns = data.columns.size - 1
-    # dp.plot.area([0, 1])
-    data_array = data.values
-    dates = data_array[:, 0]
-    close = data_array[:, 1].astype("float64")
-    low = data.values[:, 2].astype("float64")
-    quota = PluguisRepeatedlyMacdChangeQuotaForList(500)
-    start = time.clock()
-    for i in range(0, len(close)):
-        _start_time = time.clock()
-        quota.calculate(close[i], low[i], dates[i])
-        print("now index is %s,status code is %s,time: %s" % (i, quota.status, (time.clock() - _start_time)))
-        if quota.status == 0:
-            array = quota.out_put()
-            print(array)
-            data1 = pd.DataFrame(array)
-            data1.to_csv('data1.csv')
-            return
-    end = time.clock()
-    print end - start	
+	dates = np.zeros(1000)
+	close = np.zeros(1000)
+	low = np.zeros(1000)
+	quota = PluguisRepeatedlyMacdChangeQuotaForList(500)
+	start = time.clock()
+	for i in range(0, len(close)):
+		_start_time = time.clock()
+		quota.calculate(close[i], low[i], dates[i])
+		print("now index is %s,status code is %s,time: %s" % (i, quota.status, (time.clock() - _start_time)))
+		if quota.status == 0:
+			array = quota.out_put()
+			print(array)
+			data1 = pd.DataFrame(array)
+			data1.to_csv('data1.csv')
+			return
+	end = time.clock()
+	print end - start
+
 	
 def rolling_max(arr, period):
     return pd.rolling_max(arr, window=period)
@@ -110,7 +102,7 @@ def initialize(context):
     context.signal.has_open_long_position = False
     context.signal.has_open_short_position = False
     context.signal.trade_size = 1
-	run_test()
+    run_test()
 '''
 callback when position is received
   if request_id > 0     this is callback of 'req_position',
